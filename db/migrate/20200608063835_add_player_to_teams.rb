72bf768b9dddfc1e00ba545168f3cb37d4a3f17d
JB hewitt <r6anti@streamrelay.vimme.com.au>
2020-06-12 11:50:25 +0000

    fixed add_column

--- db/migrate/20200608063835_add_player_to_teams.rb
+++ db/migrate/20200608063835_add_player_to_teams.rb
@@ -1,6 +1,6 @@
 class AddPlayerToTeams < ActiveRecord::Migration[6.0]
   def change
-	  add_colmun :logs, :client_version, :string
+	  add_column :logs, :client_version, :string
 	  add_column :players, :phash, :string
   end
 end
