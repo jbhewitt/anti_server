0a2dbec85886f7d18fc97fe4090e7c6628c53deb
JB hewitt <r6anti@streamrelay.vimme.com.au>
2020-06-14 07:09:35 +0000

    sprokcets

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :deploy_to, "/var/www/r6anti"
 append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", "config/credentials"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public", "config"
 
 
 
